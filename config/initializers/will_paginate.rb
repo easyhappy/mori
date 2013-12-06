438a71b2ceb4648af7f35b1cba9c82959ef8f45c
AndyHu <hushibei@tianji.com>
2013-12-06 18:26:03 +0800

    解决will_paginate 和kaminari共存问题

---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,11 @@
+module WillPaginate
+  module ActiveRecord
+    module RelationMethods
+      def per(value = nil) per_page(value) end
+      def total_count() count end
+    end
+  end
+  module CollectionMethods
+    alias_method :num_pages, :total_pages
+  end
+end
