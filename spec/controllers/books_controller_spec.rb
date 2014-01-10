a72f8d445ee3ba6ffd85715f3d673557fca52815
AndyHu <hushibei@tianji.com>
2014-01-10 11:58:47 +0800

    添加books 测试

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
 describe BooksController do
+  before(:each) do
+    controller.stub(:current_user)
+  end
+
+  describe 'index' do
+    context 'show index' do
+      it 'should show all books' do
+        get :index
+        response.should be_success
+      end
+    end
+  end
 end
