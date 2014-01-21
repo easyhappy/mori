25ac44fbc7d74402fbb203f8160943dae327f0f5
AndyHu <hushibei@tianji.com>
2014-01-21 16:48:07 +0800

    No message.

--- lib/scraper/ranwen/contents/init_handler.rb
+++ lib/scraper/ranwen/contents/init_handler.rb
@@ -12,22 +12,24 @@ module Contents
           chapter.scraper_status = :close
           chapter.save
         rescue Exception => e
+          binding.pry
           logger_write e.inspect
         end
       end
     end
 
     def parse_content chapter, try_count=3
-      request = Typhoeus::Request.new(chapter.url)
+      request = Typhoeus::Request.new(chapter.url, :proxy => '59.39.145.178:3128')
       Typhoeus::Hydra.hydra.queue request
       Typhoeus::Hydra.hydra.run
 
       response = request.response
+      binding.pry
       return unless response.code == 200
       body = Nokogiri::HTML(response.body, nil, get_encoding)
       html = body.at_css("#content").inner_html
       pre_url, next_url = get_urls(chapter, body, 0, 2)
-
+      binding.pry
       Content.create! content: html, book_id: chapter.book_id,
                         chapter_id: chapter.id, word_count: content_count(html),
                         pre_url: pre_url, next_url: next_url
@@ -39,36 +41,6 @@ module Contents
         url = body.at_css("#thumb").children[position].attributes["href"].value
         url == 'index.html' ? '' : [base_url, url].join('/')
       end
-    end
-
-    def parse_content_old chapter
-      doc   = h chapter.url,get_encoding
-      html  = (doc/"#content").inner_html
-      begin
-        html = html.sub """\r\n\r\n<div align=\"center\"><script src=\"/ssi/style-gg.js\" type=\"text/javascript\"></script></div> \r\n\t\t\t""",""
-        html = html.sub "\r\n\t\t\t",''
-      rescue Exception => e
-        logger_write e.inspect
-      end
-
-      base_url = chapter.url.rpartition('/')[0]
-      
-      paginations = doc/'div#fenye/div#thumb/a'
-      pre_url = (la (paginations.first))[0]
-      next_url = (la (paginations[2]))[0]
-
-      pre_url = pre_url == 'index.html'  ?   '' : [base_url, pre_url].join('/')
-      next_url = next_url  == 'index.html' ? '' : [base_url, next_url].join('/')
-      Content.create! content: html, book_id: chapter.book_id,
-                        chapter_id: chapter.id, word_count: content_count(html),
-                        pre_url: pre_url, next_url: next_url
-      return chapter if chapter.name.present?
-
-      title = t doc/"h1.bname_content"
-      title = $1 if title =~ /《.*》\s*(.*?)/
-      
-      chapter.name = title
-      chapter
     end
   end
 end
