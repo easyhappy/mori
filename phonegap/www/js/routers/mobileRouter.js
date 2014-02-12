d46974684dd11cb147fa0c30927e39b143ef0590
AndyHu <hushibei@tianji.com>
2014-02-12 14:11:59 +0800

    实现mobile 切换页面 demo

--- phonegap/www/js/routers/mobileRouter.js
+++ phonegap/www/js/routers/mobileRouter.js
@@ -1,5 +1,26 @@
 define([ "jquery","backbone", "../models/CategoryModel", "../collections/CategoriesCollection", "../views/CategoryView" ], function( $, Backbone, CategoryModel, CategoriesCollection, CategoryView ) {
     // Extends Backbone.Router
+
+    window.HomeView = Backbone.View.extend({
+
+    template:_.template($('#home').html()),
+
+    render:function (eventName) {
+        $(this.el).html(this.template());
+        return this;
+    }
+  });
+
+  window.Page1View = Backbone.View.extend({
+  
+      template:_.template($('#page1').html()),
+  
+      render:function (eventName) {
+          $(this.el).html(this.template());
+          return this;
+      }
+  });
+
     var CategoryRouter = Backbone.Router.extend( {
     // The Router constructor
     initialize: function() {
@@ -12,12 +33,16 @@ define([ "jquery","backbone", "../models/CategoryModel", "../collections/Categor
     // Backbone.js Routes
     routes: {
       "": "home",
-      "category?:type": "category"
+      "page1":"page1"
     },
 
-    // Home method
-    home: function() {
-      $.mobile.changePage( "#categories" , { reverse: false, changeHash: false } );
+    home:function () {
+      this.changePage(new HomeView());
+    },
+
+    page1:function () {
+      console.log('#page1');
+      this.changePage(new Page1View());
     },
 
     category: function(type) {
@@ -31,6 +56,19 @@ define([ "jquery","backbone", "../models/CategoryModel", "../collections/Categor
       else {
         $.mobile.changePage( "#" + type, { reverse: false, changeHash: false } );
       }
+    },
+
+    changePage:function (page) {
+        $(page.el).attr('data-role', 'page');
+        page.render();
+        $('body').append($(page.el));
+        var transition = $.mobile.defaultPageTransition;
+        // We don't want to slide the first page
+        if (this.firstPage) {
+            transition = 'none';
+            this.firstPage = false;
+        }
+      $.mobile.changePage($(page.el), {changeHash:false, transition: transition});
     }
   });
   return CategoryRouter;
