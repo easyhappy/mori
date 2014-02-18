bcfa380fd418f87b051a2acc4c45f9e4bb9b1423
AndyHu <hushibei@tianji.com>
2014-02-18 14:27:23 +0800

    No message.

--- phonegap/www/js/views/BookView.js
+++ phonegap/www/js/views/BookView.js
@@ -17,19 +17,19 @@ define([ "jquery", "backbone","models/BookModel", "collections/BooksCollection"]
         this.$el.find("ul.category").attr('data-cid',  1)
         $.mobile.changePage($(this.el), {changeHash:false, transition: $.mobile.defaultPageTransition});  
       }else{
-        this.template = _.template($('ul.category').html());
         this.$el.html(this.template());
         var categoryView = _.template($('#BookItems').html(), {collection: this.collection})
         this.$el.find("ul.category").append(categoryView);
+        $('body').append($(this.el));
         $('a.category').addClass("ui-btn-active")
         $('#category_name').html(arguments[0]['category_name'])
         this.$el.find("ul.category").attr('data-page', arguments['0']['page'])
         this.$el.find("ul.category").attr('data-cid',  1)
-        var self = this;
+        $.mobile.changePage($(this.el), {changeHash:false, transition: $.mobile.defaultPageTransition});
+        var self = this 
         this.$el.find('#more_books').click(function(){
           self.more_books();
         });
-        $.mobile.changePage($(this.el), {changeHash:false, transition: 'slideup'});  
       }
       
       return this;
@@ -42,7 +42,7 @@ define([ "jquery", "backbone","models/BookModel", "collections/BooksCollection"]
         options = {dataType: 'json', data: {'cid': 1}};
         //options[data]['page'] = 
         options['data']['more'] = true
-        options['data']['page'] = $('ul.category').attr('data-page')
+        options['data']['page'] = $('ul.category').last().attr('data-page')
         //_.each(arguments, function(arg){b = arg.split("="); options['data'][b[0]] = b[1]})
         var self = this;
         currentView.collection.fetch(options).done(function(){
