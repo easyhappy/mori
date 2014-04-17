fed1531a2e66ad86ff57088168ef52867ac971a6
AndyHu <hushibei@tianji.com>
2014-04-17 15:28:39 +0800

    No message.

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,7 +50,7 @@ namespace :deploy do
     on roles(:web), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      execute :echo,  "hello andy"
+      info "Host #{host} (#{host.roles.to_a.join(', ')}):\t#{capture(:uptime)}"
       execute :rails, "s -p 8080"
     end
   end
