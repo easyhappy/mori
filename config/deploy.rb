d655a2560567a6531185fb764b62a53523d4ab66
AndyHu <hushibei@tianji.com>
2014-04-17 14:37:54 +0800

    添加rails s

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,6 +42,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
+      execute :rails, "s -p 8080"
     end
   end
 
